91fd78ad2b1decde8ded4fab86790ae7f17794a6
Ticia <ticiarodela@gmail.com>
2020-02-17 21:41:52 -0700

    restarted

--- game.js
+++ game.js
@@ -1,12 +1,47 @@
-console.log("loaded");
 
-var arr = [1,2,3,4,5];
-var item = arr[Math.floor(Math.random() * arr.length)]
-console.log(item)
+var won = 0;
+var lost = 0;
+var attempts = 10;
+var usedArray = [];
+var ranLetter = ranLetter;
+var letters = "abcdefghijklmnopqrstuvwxyz"
 
-function random(){
-    item = arr[Math.floor(Math.random() * arr.length)]
-    console.log(item) 
+ranLetter = letters[Math.floor(Math.random() * letters.length)];
+console.log(ranLetter);
+
+function jsGuess() {
+      ranLetter = letters[Math.floor(Math.random() * letters.length)];
+      console.log(ranLetter);
 }
+document.onkeyup = function (event) {
+      var playerGuess = event.key;
 
-random()
+      if (playerGuess === ranLetter) {
+            won++;
+            attempts = 10;
+            usedArray = [];
+      }
+
+      jsGuess();
+      if (playerGuess !== ranLetter) {
+            attempts--;
+
+      }
+
+    
+      if (attempts == 0) {
+            lost++;
+            usedArray = []
+            attempts = 10;
+      } 
+      else {
+            usedArray.push(playerGuess);
+            document.getElementById('playerGuess').innerHTML = usedArray;
+            console.log(usedArray);
+
+      }
+      document.getElementById('won').innerHTML = won;
+      document.getElementById('lost').innerHTML = lost;
+      document.getElementById('attempts').innerHTML = attempts;
+
+}
